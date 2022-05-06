85aea7c792458b620e5fa0e3853240a9a7313d6f
Farzan-I <farzan_15@hotmail.co.uk>
2022-05-06 18:09:28 +0100

    Step 21: Finished HP implementation

--- battle/app.rb
+++ battle/app.rb
@@ -10,11 +10,20 @@ class Battle < Sinatra::Base
     erb :index
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  enable :sessions
+
+  post '/names' do    
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
+
 
   run! if app_file == $0
 end
--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -10,3 +10,13 @@ feature "Enter names" do
     expect(page).to have_content "Farzan will DEFINITELY lose to Laura"
   end
 end
+
+feature "View HP" do
+  scenario "Player 1 views Player 2's HP" do
+    visit("/")
+    fill_in :player_1_name, with: "Farzan"
+    fill_in :player_2_name, with: "Laura"
+    click_button "Submit"
+    expect(page).to have_content 'Laura: 100HP'
+  end
+end
